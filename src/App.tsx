2149913a0347abe62dcb4ae66c5958a1021467f0
Jonathan Correa Paiva <jonathan.correa@bqn.com.uy>
2023-10-09 15:06:23 -0300

    update: refactor AdviceCard, added open.graph metadata.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 import { Box, VStack } from '@chakra-ui/react'
-import AdviceCard, { Advice } from './components/AdviceCard'
-
-
-const API_PATH = 'https://api.adviceslip.com/advice'
-
-const getAdvice = async () => {
-  return fetch(API_PATH)
-    .then(res => res.json())
-    .then(advice => advice.slip)
-    .catch(e => { throw new Error(e) })
-}
+import AdviceCard from './components/AdviceCard'
 
 function App() {
-  const [advice, setAdvice] = useState<Advice>({ id: 117, advice: "It is easy to sit up and take notice, whatś difficult is getting up and taking action." })
-
-  const handleNewAdvice = () => {
-    getAdvice().then(advice => setAdvice(advice))
-  }
 
   return (
     <VStack alignItems={'center'} justifyContent={'center'} h={'100vh'} w={'100vw'} bgColor={'hsl(218, 23%, 16%)'} >
-      <AdviceCard advice={advice} onClick={handleNewAdvice}/>
+      <AdviceCard/>
 
       <Box className="attribution" justifySelf={'flex-end'} >
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
--- src/components/AdviceCard.tsx
+++ src/components/AdviceCard.tsx
@@ -1,24 +1,41 @@
 import { Box, BoxProps, IconButton, Image, Text, VStack } from "@chakra-ui/react"
 import Dice from '../assets/icon-dice.svg'
 import dDivider from '../assets/pattern-divider-desktop.svg'
+import { useState } from "react"
 // import mDivider from '../assets/pattern-divider-mobile.svg'
 
 
 export
     type Advice = {
         id: number,
-        advice: string
+        text: string
     }
 
 interface AdviceCardProps extends BoxProps {
-    onClick: () => void
-    advice: Advice
+    onClick?: () => void
+}
+
+const API_PATH = 'https://api.adviceslip.com/advice'
+
+const getAdvice = async () => {
+  return fetch(API_PATH)
+    .then(res => res.json())
+    .then(advice => { return {
+        id: advice.slip.id,
+        text: advice.slip.advice}
+    })
+    .catch(e => { throw new Error(e) })
 }
 
 const AdviceCard = (props: AdviceCardProps) => {
+  const [advice, setAdvice] = useState<Advice>({ id: 117, text: "It is easy to sit up and take notice, whatś difficult is getting up and taking action." })
+
+    const handleNewAdvice = () => {
+        getAdvice().then(advice => setAdvice(advice))
+    }
 
     return (
-        <Box
+        <Box onClick={props.onClick}
             w={'90vw'}
             maxW={'580px'}
             {...props} >
@@ -27,8 +44,8 @@ const AdviceCard = (props: AdviceCardProps) => {
                 p={'2.5rem'}
                 style={{ borderRadius: '1rem', boxShadow: '0px 0px 100px 20px rgba(0,0,0,.15)' }}>
                 <VStack gap={'1.5rem'} >
-                    <Text fontFamily='Manrope, sans-serif' letterSpacing={'.3rem'} fontWeight='800' fontSize={'12px'} color={'hsl(150, 100%, 66%)'}>ADVICE #{props.advice.id}</Text>
-                    <Text fontFamily='Manrope, sans-serif' fontWeight='800' color={'hsl(193, 38%, 86%)'} fontSize={'28px'}>"{props.advice.advice}"</Text>
+                    <Text fontFamily='Manrope, sans-serif' letterSpacing={'.3rem'} fontWeight='800' fontSize={'12px'} color={'hsl(150, 100%, 66%)'}>ADVICE #{advice.id}</Text>
+                    <Text fontFamily='Manrope, sans-serif' fontWeight='800' color={'hsl(193, 38%, 86%)'} fontSize={'28px'}>"{advice.text}"</Text>
                     <Image src={dDivider} my={'1rem'} />
                 </VStack>
             </Box>
@@ -40,7 +57,7 @@ const AdviceCard = (props: AdviceCardProps) => {
                 bgColor={'hsl(150, 100%, 66%)'}
                 _hover={{ backgroundColor: 'hsl(150, 100%, 66%)', boxShadow: '0px 0px 20px 5px hsl(150, 100%, 66%, 45%)' }}
                 style={{ borderRadius: '50%' }}
-                onClick={props.onClick} />
+                onClick={handleNewAdvice} />
         </Box>
     )
 }
